d7eff2f6ac920c2c98f35703c089948d903fadf9
Lukas Rappen <lukasrappen@Lukass-MacBook-Air.local>
2023-12-20 16:26:18 +0200

    mvc folder setup

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world.");
+// listen to route '/hello' on port 5000
+const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("hello world.");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on PORT:${port}.`);
+});
